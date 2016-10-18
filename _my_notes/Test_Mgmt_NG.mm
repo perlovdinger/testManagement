a5bb4a2a741a7bb15a74009921844303017ea3db
Per Lövdinger <per.lovdinger@volvo.com>
2016-10-18 15:28:47 +0200

    first commit

---
+++ _my_notes/Test_Mgmt_NG.mm
@@ -0,0 +1,207 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1476177302966" ID="ID_1730115274" MODIFIED="1476185792646" TEXT="Test_Mgmt_NG">
+<node CREATED="1476177493710" ID="ID_1230004426" MODIFIED="1476177498079" POSITION="right" TEXT="people">
+<node CREATED="1476701523447" ID="ID_772579732" MODIFIED="1476701733141" TEXT="FVV (Feature Verification &amp; Validation">
+<node CREATED="1476701523450" ID="ID_1196547641" MODIFIED="1476701523450" TEXT="Global Virtual &amp; Physical Testing Technology(Lars Forsman)">
+<node CREATED="1476701523453" ID="ID_1768963462" MODIFIED="1476701523453" TEXT="PVT(">
+<node CREATED="1476701523456" MODIFIED="1476701523456" TEXT="Arto Mattila - Global PVT Manager"/>
+<node CREATED="1476701523459" MODIFIED="1476701523459" TEXT="Johan Jacobson - PVT Test Engineer"/>
+<node CREATED="1476701523460" MODIFIED="1476701523460" TEXT="Therese - PVT test Engineer"/>
+<node CREATED="1476177724591" ID="ID_1234990643" MODIFIED="1476177726517" TEXT="Jansson Tim tim.jansson@volvo.com">
+<node CREATED="1476177809033" ID="ID_1210309448" MODIFIED="1476177810749" TEXT="Truck function Specialist,"/>
+</node>
+</node>
+<node CREATED="1476701523462" MODIFIED="1476701523462" TEXT="AET\RT">
+<node CREATED="1476701523463" MODIFIED="1476701523463" TEXT="Axel Vallerby"/>
+<node CREATED="1476701523466" MODIFIED="1476701523466" TEXT="Tomas Voigt"/>
+</node>
+<node CREATED="1476701523467" MODIFIED="1476701523467" TEXT="Safety">
+<node CREATED="1476701523469" MODIFIED="1476701523469" TEXT="Magnus Str&#xf6;m- Test Engineer Active Safety"/>
+<node CREATED="1476701523471" MODIFIED="1476701523471" TEXT="Henrik Lindh"/>
+</node>
+<node CREATED="1476701523473" MODIFIED="1476701523473" TEXT="Martin Svennungsson - EFACTS">
+<node CREATED="1476701523474" MODIFIED="1476701523474" TEXT="possible &quot;product owner&quot;"/>
+<node CREATED="1476701523476" MODIFIED="1476701523476" TEXT="Global measurment technology manager"/>
+</node>
+<node CREATED="1476701523478" MODIFIED="1476701523478" TEXT="Jonas Quist - EFACTS">
+<node CREATED="1476701523480" MODIFIED="1476701523480" TEXT="Global testdata Manager"/>
+</node>
+<node CREATED="1476701523482" MODIFIED="1476701523482" TEXT="Magnus Hansson -RDM">
+<node CREATED="1476701523484" MODIFIED="1476701523484" TEXT="Global Remote data manager"/>
+</node>
+<node CREATED="1476701523486" MODIFIED="1476701523486" TEXT="Jessica Erlandsson - Global Function Testing Manager"/>
+</node>
+<node CREATED="1476179185506" ID="ID_634033509" MODIFIED="1476179266254">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>FVV - Features, Verification &amp; Validation</b>
+    </p>
+    <p>
+      FVV is responsible for defining measurable customer requirements in early project phases,
+    </p>
+    <p>
+      as well as working together with Engineering to break down customer features at both system
+    </p>
+    <p>
+      and module levels.
+    </p>
+    <p>
+      We also manage validation plans to reach requested feature level and customer satisfaction.
+    </p>
+    <p>
+      Furthermore, FVV manages verification plans in partnership with engineering teams,
+    </p>
+    <p>
+      and provides optimized and world-class tools such as calculations, rigs, dynos, proving
+    </p>
+    <p>
+      grounds, and field tests.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1476701523487" ID="ID_75058580" MODIFIED="1476701523487" TEXT="Lars B&#xf6;rjesson - Manager Process Verification&#xff06;Validation">
+<node CREATED="1476701523495" MODIFIED="1476701523495" TEXT="Magnus S&#xf6;der(BSPM)"/>
+</node>
+<node CREATED="1476701523498" ID="ID_288443336" MODIFIED="1476701774260" TEXT="Ulf Krysell - GTDM (Global Testdata Management)"/>
+<node CREATED="1476701523500" ID="ID_1004550950" MODIFIED="1476701523500" TEXT="Ulf krysell - TCP(Test Collaboration Platform)">
+<node CREATED="1476701523502" MODIFIED="1476701523502" TEXT="RDM\EFACTS\test manger">
+<node CREATED="1476701523504" MODIFIED="1476701523504" TEXT="Fabrice De Meec - solution leader"/>
+<node CREATED="1476701523507" MODIFIED="1476701523507" TEXT="Raphael Scotton- Delivery Manager V&amp;V"/>
+<node CREATED="1476701523508" MODIFIED="1476701523508" TEXT="Pooja Shetty - Team lead ?"/>
+<node CREATED="1476701523509" ID="ID_839570660" MODIFIED="1476701523509" TEXT="Johan Uneman - Team leader EFACTS"/>
+</node>
+</node>
+<node CREATED="1476701523511" ID="ID_693994404" MODIFIED="1476701523511" TEXT="Protus\Protom"/>
+<node CREATED="1476701523513" ID="ID_1547969349" MODIFIED="1476701523513" TEXT="KOLA"/>
+</node>
+<node CREATED="1476787135202" ID="ID_863295750" MODIFIED="1476787140210" POSITION="right" TEXT="pre-study">
+<node CREATED="1476186328750" ID="ID_476212726" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\projektbeskrivning\A3 EFACTS\A3 Tool for CV Testing has become out_of_date.pptx" MODIFIED="1476186339926" TEXT="A3 Tool for CV Testing has become out_of_date.pptx"/>
+</node>
+<node CREATED="1476791550892" ID="ID_1905481578" MODIFIED="1476796068769" POSITION="right" TEXT="process">
+<node CREATED="1476791554584" ID="ID_406862565" MODIFIED="1476791554584" TEXT="V&amp;V plan">
+<node CREATED="1476791554584" MODIFIED="1476791554584" TEXT="acceptance criteria &amp; methods"/>
+<node CREATED="1476791554584" MODIFIED="1476791554584" TEXT="select TestObject"/>
+<node CREATED="1476791554584" MODIFIED="1476791554584" TEXT="time plan"/>
+<node CREATED="1476791554584" MODIFIED="1476791554584" TEXT="cost estimate,risk,PDP"/>
+</node>
+<node CREATED="1476791554584" ID="ID_403328069" MODIFIED="1476791554584" TEXT="Test request">
+<node CREATED="1476791554584" MODIFIED="1476791554584" TEXT="parts"/>
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="material"/>
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="resources"/>
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="testobject build"/>
+</node>
+<node CREATED="1476791554600" ID="ID_681173205" MODIFIED="1476791554600" TEXT="Plan activities &amp; resources"/>
+<node CREATED="1476791554600" ID="ID_1467679244" MODIFIED="1476791554600" TEXT="Prepare for test">
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="ex: testobject prepared with measurement equip"/>
+</node>
+<node CREATED="1476791554600" ID="ID_1656695587" MODIFIED="1476791554600" TEXT="Perform test">
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="Test Performed">
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="test procedures"/>
+</node>
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="collect/analyse measured info"/>
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="Report test Result">
+<node CREATED="1476791554600" MODIFIED="1476791554600" TEXT="Protus lab report"/>
+<node CREATED="1476791554616" MODIFIED="1476791554616" TEXT="Status report"/>
+<node CREATED="1476791554616" MODIFIED="1476791554616" TEXT="Engineering Report"/>
+</node>
+</node>
+</node>
+<node CREATED="1476787113015" FOLDED="true" ID="ID_1482715368" MODIFIED="1476791573289" POSITION="right" TEXT="AS-IS">
+<node CREATED="1476186820918" ID="ID_1749839245" MODIFIED="1476186825996" TEXT="business presentations">
+<node CREATED="1476186358029" ID="ID_1145106366" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\projektbeskrivning\FVV Presentation.pptx" MODIFIED="1476186367730" TEXT="FVV Presentation.pptx"/>
+<node CREATED="1476186381735" ID="ID_1845011393" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\projektbeskrivning\20140402 Presentation f&#xf6;r f&#xe4;ltprov.pptx" MODIFIED="1476186391258" TEXT="20140402 Presentation f&#xf6;r f&#xe4;ltprov.pptx"/>
+</node>
+<node CREATED="1476177626618" ID="ID_1120567081" MODIFIED="1476787197960" TEXT="applications">
+<node CREATED="1476177644147" ID="ID_640885207" MODIFIED="1476787252590" TEXT="eFACTS &amp; RDM">
+<node COLOR="#006699" CREATED="1476187216695" ID="ID_328680561" MODIFIED="1476187290704">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left; margin-top: 0pt; margin-left: 0in; margin-bottom: 0pt">
+      Web interface for remote retrieval of test &amp; measurement data from
+    </p>
+    <p style="text-align: left; margin-top: 0pt; margin-left: 0in; margin-bottom: 0pt">
+      test vehicles around the world and configuration of on-board data loggers
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1476186486036" ID="ID_1298195042" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Architectual reviews\System holistic view\eFacts_RDM.pptx" MODIFIED="1476186497032" TEXT="eFacts_RDM.pptx"/>
+<node CREATED="1476186581899" ID="ID_1035473894" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Architectual reviews\EA Review Report -RDM (2).docx" MODIFIED="1476186595831" TEXT="EA Review Report -RDM (2).docx"/>
+<node CREATED="1476787371637" ID="ID_13114803" MODIFIED="1476787379079" TEXT="EFACTS">
+<node COLOR="#006699" CREATED="1476187343189" ID="ID_537349344" MODIFIED="1476787325409">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      eFacts
+    </p>
+    <p>
+      Database and tool to take care of incoming field
+    </p>
+    <p>
+      test data for functional testing often software
+    </p>
+    <p>
+      based services
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1476186552094" ID="ID_1173476730" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Architectual reviews\Architecture Review - eFACTS_151002.docx" MODIFIED="1476186561430" TEXT="Architecture Review - eFACTS_151002.docx"/>
+<node CREATED="1476186446902" ID="ID_283413528" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Architectual reviews\System holistic view\eFACTS_basic.pptx" MODIFIED="1476186458638" TEXT="eFACTS_basic.pptx"/>
+</node>
+<node CREATED="1476187409563" ID="ID_1970189689" MODIFIED="1476187410733" TEXT="EFACTS Refiner">
+<node CREATED="1476187437284" ID="ID_1969733333" MODIFIED="1476187439072" TEXT="Solution to be able to refine/review retrieved test &amp; measurement data"/>
+</node>
+</node>
+<node CREATED="1476177664061" ID="ID_1377825878" MODIFIED="1476177665278" TEXT="PVT Manager ">
+<node CREATED="1476187498712" ID="ID_58112708" MODIFIED="1476187523856">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tool to support Tester with Test Codes, Activities &amp; Reports
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1476787128484" FOLDED="true" ID="ID_293796971" MODIFIED="1476791575442" POSITION="right" TEXT="TO_BE">
+<node CREATED="1476186666214" ID="ID_1618543192" MODIFIED="1476186951255" TEXT="project Description">
+<node CREATED="1476186685621" ID="ID_98437328" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Project description\160602 Next Gen RDM EFACTS TESTMANAGER Pre Study v1.2.pptx" MODIFIED="1476186696533" TEXT="160602 Next Gen RDM EFACTS TESTMANAGER Pre Study v1.2.pptx"/>
+<node CREATED="1476186714921" ID="ID_429463280" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Project description\PVT_Step2.pptx" MODIFIED="1476186765551" TEXT="PVT_Step2.pptx"/>
+<node CREATED="1476186794818" ID="ID_1701534237" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Project description\Step2_mockup_arto.pptx" MODIFIED="1476186808146" TEXT="Step2_mockup_arto.pptx"/>
+</node>
+<node CREATED="1476186953167" ID="ID_1000104404" MODIFIED="1476186960122" TEXT="Requirement work">
+<node CREATED="1476186962933" ID="ID_1330571096" LINK="H:\My Documents\_WorkH\NG RDM_EFACTS_Test Manager\Requirements work\Requirement work\Presentation of requirements work.pptx" MODIFIED="1476186986872" TEXT="Presentation of requirements work.pptx"/>
+</node>
+</node>
+<node CREATED="1476185793459" ID="ID_1643193573" MODIFIED="1476185798342" POSITION="right" TEXT="Docs">
+<node CREATED="1476186841359" ID="ID_1380214022" MODIFIED="1476187134477" TEXT="solution areas">
+<node CREATED="1476187084963" ID="ID_536239004" MODIFIED="1476187097755" TEXT="functional test"/>
+<node CREATED="1476187162356" ID="ID_1247839902" MODIFIED="1476187164424" TEXT="AET/RT"/>
+</node>
+</node>
+<node CREATED="1476189524044" ID="ID_966017821" MODIFIED="1476189525353" POSITION="right" TEXT="notes">
+<node CREATED="1476189526449" ID="ID_40307928" MODIFIED="1476189531091" TEXT="M-logger"/>
+<node CREATED="1476189532984" ID="ID_903947666" MODIFIED="1476189716484" TEXT="Can bus"/>
+<node CREATED="1476189718917" ID="ID_380376906" MODIFIED="1476189728210" TEXT="Can files"/>
+</node>
+</node>
+</map>
