2af44daa14068db6981270e571281646c5e5d8dc
Per Lövdinger <per.lovdinger@volvo.com>
2017-01-12 09:11:56 +0100

    Way of working

--- 0_PrePreStudy/PrePrestudy.mm
+++ 0_PrePreStudy/PrePrestudy.mm
@@ -2,8 +2,8 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1478632739879" ID="ID_1185136024" MODIFIED="1478643982212" TEXT="Uppdraget">
 <node CREATED="1478633046244" ID="ID_593509026" MODIFIED="1478680623822" POSITION="right" TEXT="input">
-<node CREATED="1478632820865" ID="ID_292320798" LINK="H:\My Documents\_WorkH\_Test Mgmt_NG\5_To_Be\1_prestudy\A3 Tool for CV Testing has become out_of_date.pptx" MODIFIED="1479124832797" TEXT="A3 Tool for CV Testing has become out_of_date.pptx">
-<node CREATED="1478632855880" ID="ID_1325434310" MODIFIED="1478632894565" TEXT="Current Situation">
+<node CREATED="1478632820865" FOLDED="true" ID="ID_292320798" LINK="H:\My Documents\_WorkH\_Test Mgmt_NG\5_To_Be\1_prestudy\A3 Tool for CV Testing has become out_of_date.pptx" MODIFIED="1484205688930" TEXT="A3 Tool for CV Testing has become out_of_date.pptx">
+<node CREATED="1478632855880" ID="ID_1325434310" MODIFIED="1484204185872" TEXT="Current Situation">
 <node CREATED="1478632908668" ID="ID_746672927" MODIFIED="1478632909632" TEXT="Applications are not reliable "/>
 <node CREATED="1478632918824" ID="ID_1461277331" MODIFIED="1478632922283" TEXT="Continuous improvements and new ways of working not supported "/>
 <node CREATED="1478632957837" ID="ID_1884417463" MODIFIED="1478632958396" TEXT="Inefficient work flow. "/>
@@ -44,13 +44,54 @@
 <node CREATED="1478680764110" ID="ID_523408505" MODIFIED="1478680769097" TEXT="46 pages"/>
 </node>
 <node CREATED="1478635094525" ID="ID_443546291" LINK="H:\My Documents\_WorkH\_Test Mgmt_NG\5_To_Be\2_Business_Requirement\PVT_Step2.pptx" MODIFIED="1478682787147" TEXT="PVT_Step2.pptx">
-<node CREATED="1478680831993" ID="ID_463451788" MODIFIED="1478680835213" TEXT="20 pages"/>
+<node CREATED="1484204696347" ID="ID_901534958" MODIFIED="1484204697564" TEXT="New functionality have been frequently added as well as the global complexity "/>
+<node CREATED="1484204718069" ID="ID_227078144" MODIFIED="1484204719036" TEXT="The current tools can be used as a good reference, new foundation is critical "/>
+<node CREATED="1478632957837" ID="ID_1283135994" MODIFIED="1478632958396" TEXT="Inefficient work flow. ">
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Require a lot of manual work"/>
+<node CREATED="1484205117095" ID="ID_40013244" MODIFIED="1484205117095" TEXT="Many hours can be spent to correct a single mistake by a user done during the manual work."/>
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Tools are hard to understand (developed for approx 10 users)"/>
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Managing test cases require more than one resource 100%"/>
+</node>
 </node>
 <node CREATED="1478633551342" ID="ID_778336874" LINK="H:\My Documents\_WorkH\_Test Mgmt_NG\5_To_Be\2_Business_Requirement\Step2_mockup_arto.pptx" MODIFIED="1478682813016" TEXT="Step2_mockup_arto.pptx">
 <node CREATED="1478680792190" ID="ID_643611320" MODIFIED="1478680795110" TEXT="34 pages"/>
 </node>
 <node COLOR="#ff0000" CREATED="1478681265401" ID="ID_1884343850" MODIFIED="1478681327887" TEXT="Other relevant docs ??"/>
 <node CREATED="1478685388655" ID="ID_1763219123" MODIFIED="1478685390716" TEXT="meetings"/>
+</node>
+<node CREATED="1484203993630" ID="ID_1799330629" MODIFIED="1484205297809" POSITION="right" TEXT="requirement-targets">
+<node CREATED="1484204103820" ID="ID_558390595" MODIFIED="1484204114667" TEXT="Reliability">
+<node CREATED="1478632908668" ID="ID_1150326989" MODIFIED="1478632909632" TEXT="Applications are not reliable "/>
+</node>
+<node CREATED="1484204132103" ID="ID_1219789801" MODIFIED="1484204140718" TEXT="Productivity">
+<node CREATED="1478632957837" ID="ID_1743530342" MODIFIED="1478632958396" TEXT="Inefficient work flow. ">
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Require a lot of manual work"/>
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Many hours can be spent to correct a single mistake by a user done during the manual work."/>
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Tools are hard to understand (developed for approx 10 users)"/>
+<node CREATED="1484205117095" MODIFIED="1484205117095" TEXT="Managing test cases require more than one resource 100%"/>
+</node>
+<node CREATED="1478632992399" ID="ID_329883230" MODIFIED="1478632993008" TEXT="Cut lead time "/>
+<node CREATED="1478633000132" ID="ID_1402709003" MODIFIED="1478633000671" TEXT="Improved way of working "/>
+</node>
+<node CREATED="1484204146798" ID="ID_1029879660" MODIFIED="1484204151496" TEXT="Traceability"/>
+<node CREATED="1484204158350" ID="ID_550334056" MODIFIED="1484204167501" TEXT="Extendability">
+<node CREATED="1478632918824" ID="ID_489198586" MODIFIED="1478632922283" TEXT="Continuous improvements and new ways of working not supported ">
+<node CREATED="1484204941824" ID="ID_1569811924" MODIFIED="1484204941824" TEXT="PVT test methods hard to add."/>
+<node CREATED="1484204941824" ID="ID_806920418" MODIFIED="1484204941824" TEXT="PVT Development, PVT Distribution and PVT Road Approval needs to be added, more coming"/>
+<node CREATED="1484204941824" MODIFIED="1484204941824" TEXT="New setup of test cases is needed mainly due to global diversity, efficiency and maturity."/>
+<node CREATED="1484204941824" MODIFIED="1484204941824" TEXT="Projects are unique and the product technical content keeps changing rapidly."/>
+</node>
+<node CREATED="1484204696347" ID="ID_74769696" MODIFIED="1484204697564" TEXT="New functionality have been frequently added as well as the global complexity "/>
+<node CREATED="1484204718069" ID="ID_888203690" MODIFIED="1484204719036" TEXT="The current tools can be used as a good reference, new foundation is critical "/>
+</node>
+<node CREATED="1484204352373" ID="ID_798014695" MODIFIED="1484204353590" TEXT="supporting a wider usage for other areas like AET/RT"/>
+<node CREATED="1484204516340" ID="ID_745299699" MODIFIED="1484204517807" TEXT="Tool to take care of incoming field test data for functional testing often software based services "/>
+<node CREATED="1484205305032" ID="ID_268970316" MODIFIED="1484205327213" TEXT="The system shall support flexibility, efficiency and shall be easy to adapt to global diversity, rapid technology growth in products and unique project demands">
+<node CREATED="1484205326293" ID="ID_100751842" MODIFIED="1484205326293" TEXT="Test cases are frequently edited, added and deleted at all six sites, new setup needed to keep global test value (Q, D, C, F)"/>
+<node CREATED="1484205343212" ID="ID_1502188605" MODIFIED="1484205344382" TEXT="The tools need to support new test methods and test execution. More efficient work flow and quality of information to describe symptons in products to developers and suppliers "/>
+<node CREATED="1484205357592" ID="ID_1747124462" MODIFIED="1484205358466" TEXT="The tools need to offer stability, performance, traceability, chain of activities and connections to other tools used within GTT during the development process  "/>
+<node CREATED="1484205388255" ID="ID_1374158431" MODIFIED="1484205389320" TEXT="Known changes: New functional structure, Protus (Jira?), Protom, Ghost, Collaboration platform, ITS, ISO 26262, Data Protection Law "/>
+</node>
 </node>
 </node>
 </map>
