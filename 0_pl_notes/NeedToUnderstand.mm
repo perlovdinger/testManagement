0d2de0ed5201eb2dfe2f546f240a1ed245133fde
Per Lövdinger <per.lovdinger@volvo.com>
2017-01-13 17:30:54 +0100

    x

---
+++ 0_pl_notes/NeedToUnderstand.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1484324041965" ID="ID_36762227" MODIFIED="1484324927282" TEXT="NeedToUnderstand">
+<node CREATED="1484324080158" ID="ID_1731683777" MODIFIED="1484324087291" POSITION="right" TEXT="application landscape"/>
+<node CREATED="1484324087651" ID="ID_1526987576" MODIFIED="1484324128684" POSITION="right" TEXT="Functional Requirements Spec"/>
+<node CREATED="1484324928942" MODIFIED="1484324928942" POSITION="left" TEXT="PVT_TOTAL,PVT_BASE..."/>
+<node CREATED="1484324928942" MODIFIED="1484324928942" POSITION="left" TEXT="TestCase Container"/>
+<node CREATED="1484324928942" MODIFIED="1484324928942" POSITION="left" TEXT="PVT and Software"/>
+</node>
+</map>
