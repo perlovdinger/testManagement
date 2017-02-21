9be2429f47d31e4a7c42c546a06efb808f45a1ce
Per Lövdinger <per.lovdinger@volvo.com>
2017-02-21 10:29:01 +0100

    few files

---
+++ 0_pl_notes/Requirements.mm
@@ -0,0 +1,76 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1487597126631" ID="ID_112512913" MODIFIED="1487597135582" TEXT="Requirements">
+<node CREATED="1484203993630" ID="ID_1799330629" MODIFIED="1487597169296" POSITION="right" TEXT="Non Func Reqirements">
+<node CREATED="1484204103820" ID="ID_558390595" MODIFIED="1484204114667" TEXT="Reliability">
+<node CREATED="1478632908668" ID="ID_1150326989" MODIFIED="1478632909632" TEXT="Applications are not reliable "/>
+</node>
+<node CREATED="1484816096317" ID="ID_1915528649" MODIFIED="1484817370371" TEXT="Usability">
+<node CREATED="1484815980962" ID="ID_104631137" MODIFIED="1484817394546" TEXT="minimize user input"/>
+<node CREATED="1478632957837" ID="ID_1743530342" MODIFIED="1486555784140" TEXT="Inefficient work flow. ">
+<node CREATED="1484205117095" ID="ID_1552169161" MODIFIED="1484205117095" TEXT="Require a lot of manual work"/>
+<node CREATED="1484205117095" ID="ID_326961957" MODIFIED="1484205117095" TEXT="Many hours can be spent to correct a single mistake by a user done during the manual work."/>
+<node CREATED="1484205117095" ID="ID_1935150998" MODIFIED="1484205117095" TEXT="Tools are hard to understand (developed for approx 10 users)"/>
+<node CREATED="1484205117095" ID="ID_1776221247" MODIFIED="1484205117095" TEXT="Managing test cases require more than one resource 100%"/>
+</node>
+<node CREATED="1478632992399" ID="ID_329883230" MODIFIED="1478632993008" TEXT="Cut lead time "/>
+</node>
+<node CREATED="1484204146798" ID="ID_1029879660" MODIFIED="1484204151496" TEXT="Traceability"/>
+<node CREATED="1484204158350" ID="ID_550334056" MODIFIED="1484204167501" TEXT="Extendability">
+<node CREATED="1484204352373" ID="ID_798014695" MODIFIED="1484204353590" TEXT="supporting a wider usage for other areas like AET/RT"/>
+</node>
+<node CREATED="1487597305937" FOLDED="true" ID="ID_1690344741" MODIFIED="1487597526334" TEXT="Changeability">
+<node CREATED="1478632918824" ID="ID_489198586" MODIFIED="1478632922283" TEXT="Continuous improvements and new ways of working not supported ">
+<node CREATED="1484204941824" ID="ID_1569811924" MODIFIED="1484204941824" TEXT="PVT test methods hard to add."/>
+<node CREATED="1484204941824" ID="ID_806920418" MODIFIED="1484204941824" TEXT="PVT Development, PVT Distribution and PVT Road Approval needs to be added, more coming"/>
+<node CREATED="1484204941824" ID="ID_689796054" MODIFIED="1484204941824" TEXT="New setup of test cases is needed mainly due to global diversity, efficiency and maturity."/>
+<node CREATED="1484204941824" ID="ID_776111287" MODIFIED="1484204941824" TEXT="Projects are unique and the product technical content keeps changing rapidly."/>
+</node>
+<node CREATED="1484205305032" ID="ID_268970316" MODIFIED="1484205327213" TEXT="The system shall support flexibility, efficiency and shall be easy to adapt to global diversity, rapid technology growth in products and unique project demands">
+<node CREATED="1484205326293" ID="ID_100751842" MODIFIED="1484205326293" TEXT="Test cases are frequently edited, added and deleted at all six sites, new setup needed to keep global test value (Q, D, C, F)"/>
+<node CREATED="1484205357592" ID="ID_1747124462" MODIFIED="1484205358466" TEXT="The tools need to offer stability, performance, traceability, chain of activities and connections to other tools used within GTT during the development process  "/>
+<node CREATED="1484205388255" ID="ID_1374158431" MODIFIED="1484205389320" TEXT="Known changes: New functional structure, Protus (Jira?), Protom, Ghost, Collaboration platform, ITS, ISO 26262, Data Protection Law "/>
+<node CREATED="1484205343212" ID="ID_1502188605" MODIFIED="1484205344382" TEXT="The tools need to support new test methods and test execution. More efficient work flow and quality of information to describe symptons in products to developers and suppliers "/>
+</node>
+</node>
+<node CREATED="1484210800312" ID="ID_460934806" MODIFIED="1487597252261" TEXT="Quality">
+<node CREATED="1484204696347" ID="ID_74769696" MODIFIED="1484204697564" TEXT="New functionality have been frequently added as well as the global complexity "/>
+<node CREATED="1484204718069" ID="ID_888203690" MODIFIED="1484204719036" TEXT="The current tools can be used as a good reference, new foundation is critical "/>
+</node>
+<node CREATED="1484210765811" ID="ID_493989217" MODIFIED="1484210785746" TEXT="support for software based services">
+<node CREATED="1484204516340" ID="ID_745299699" MODIFIED="1484204517807" TEXT="Tool to take care of incoming field test data for functional testing often software based services "/>
+</node>
+</node>
+<node CREATED="1484815950244" ID="ID_1155361370" MODIFIED="1484815969359" POSITION="right" TEXT="critical requirements communicated at meeting">
+<node CREATED="1484816036704" ID="ID_613628866" MODIFIED="1484816043357" TEXT="Test Cases">
+<node CREATED="1484815980957" ID="ID_1805806149" MODIFIED="1484817411628" TEXT="test case / per product"/>
+<node CREATED="1484815980969" ID="ID_1594116470" MODIFIED="1484815980969" TEXT="temporary test case"/>
+<node CREATED="1484815980959" ID="ID_1248201966" MODIFIED="1484817438555" TEXT="Other structure for Test sequence"/>
+<node CREATED="1484815980971" ID="ID_380984189" MODIFIED="1484817463258" TEXT="test case at part level ???"/>
+<node CREATED="1484815980968" ID="ID_1967325440" MODIFIED="1484817474454" TEXT="categorize test cases"/>
+<node CREATED="1484816168490" ID="ID_1065703906" MODIFIED="1484816244307" TEXT="TestCases - Requirements">
+<node CREATED="1484815980968" ID="ID_1204361722" MODIFIED="1484815980968" TEXT="kraven kopplade till FRS    -&gt; SE-tool"/>
+</node>
+<node CREATED="1484816253464" ID="ID_1677207887" MODIFIED="1484816271383" TEXT="TestCases - Software">
+<node CREATED="1484815980963" ID="ID_53547207" MODIFIED="1484815980963" TEXT="sw &#xe4;ndras i specifik ECU"/>
+</node>
+</node>
+<node CREATED="1484816385440" ID="ID_959027137" MODIFIED="1484816403555" TEXT="Info Connections">
+<node CREATED="1484815980958" ID="ID_1477226879" MODIFIED="1484816531199" TEXT="Measure Log   &lt;-&gt; Test case"/>
+<node CREATED="1484815980967" ID="ID_550077644" MODIFIED="1484816510444" TEXT="Tablet &lt;---&gt; Measure Log"/>
+<node CREATED="1484815980967" ID="ID_670947913" MODIFIED="1484815980967" TEXT="plattan kopplad till m&#xe4;t systemet"/>
+</node>
+<node CREATED="1484816480368" ID="ID_125957688" MODIFIED="1484816486092" TEXT="Measure Log"/>
+<node CREATED="1484816431574" ID="ID_1926834254" MODIFIED="1484816433398" TEXT="Other">
+<node CREATED="1484815980961" ID="ID_1326246120" MODIFIED="1484815980961" TEXT="&#xe4;ndra refiner protus"/>
+<node CREATED="1484815980969" ID="ID_1215200282" MODIFIED="1484815980969" TEXT="protus report vill rapportera p&#xe5; &quot;komponent niv&#xe5;&quot; part"/>
+</node>
+<node CREATED="1484816456313" ID="ID_872871406" MODIFIED="1484816477315" TEXT="tablet">
+<node CREATED="1484815980964" ID="ID_472257954" MODIFIED="1484815980964" TEXT="AERT g&#xe4;nget  (titta p&#xe5; appen)"/>
+</node>
+<node CREATED="1484815980964" ID="ID_1899815034" MODIFIED="1484815980964" TEXT="offline">
+<node CREATED="1484815980966" ID="ID_727097627" MODIFIED="1484815980966" TEXT="f&#xf6;r&#xe4;dla result"/>
+</node>
+</node>
+</node>
+</map>
